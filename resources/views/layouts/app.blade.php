af747d185f62eeafaaa6f858cae1ff53e53b9539
Felipe Martins Vittorazzi <felipe.vttzz@gmail.com>
2023-09-04 23:57:22 -0300

    == Closet Chic + Adicionando mascara de cpf e telefone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
@@ -169,6 +171,13 @@
         </div>
 
     </div>
+
+    <script>
+        $(document).ready(function(){
+            $('.phone').inputmask('(99) 99999-9999');
+            $('.cpf').inputmask('999.999.999-99');
+        });
+    </script>
 </body>
 
 </html>
